d8664c9bc4c45fe1aecf8e5412bc4879f10a35aa
sysbird <koro@sysbird.jp>
2016-03-22 23:07:25 +0900

    single

---
+++ page.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * The template for displaying pages
+ *
+ * @package WordPress
+ * @subpackage BirdFILED
+ * @since BirdFILED 1.0
+ */
+get_header(); ?>
+
+<div id="content">
+	<div class="container">
+
+<?php while ( have_posts() ) : the_post(); ?>
+	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+		<?php get_template_part( 'content', 'singular' ); ?>
+		<?php comments_template( '', true ); ?>
+	</article>
+<?php endwhile; ?>
+
+	</div>
+</div>
+
+<?php get_footer(); ?>
